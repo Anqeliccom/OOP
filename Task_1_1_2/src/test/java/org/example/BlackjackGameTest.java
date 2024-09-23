093f0edb814e0823ad48ce234c7d4546ba9a66d6
Angelica <khamidulina.lika@inbox.ru>
2024-09-23 14:35:50 +0700

    Add some checkstyle fixes

--- Task_1_1_2/src/test/java/org/example/BlackjackGameTest.java
+++ Task_1_1_2/src/test/java/org/example/BlackjackGameTest.java
@@ -24,22 +24,19 @@ public class BlackjackGameTest {
     public void testPlayerAlwaysTakesCard() {
         game.playRound(new Scanner("1\n1\n1\n1\n1\n1\n1\n"));
         assertTrue(game.getDeck().getCards().size() >= 41);
-        assertTrue(game.getStats().contains("1:0") || game.getStats().contains("0:1") || game.getStats().contains("0:0"));
+        assertTrue(game.getStats().contains("1:0")
+            || game.getStats().contains("0:1") || game.getStats().contains("0:0"));
 
         if (game.getStats().contains("0:0")) {
             assertEquals(21, game.getPlayer().getScore());
             assertEquals(2, game.getPlayer().getHand().size());
             assertEquals(21, game.getDealer().getScore());
             assertEquals(2, game.getDealer().getHand().size());
-        }
-
-        else if (game.getStats().contains("1:0")) {
+        } else if (game.getStats().contains("1:0")) {
             assertEquals(21, game.getPlayer().getScore());
             assertEquals(2, game.getPlayer().getHand().size());
-        }
-
-        else if (game.getStats().contains("0:1")) {
-                assertTrue(game.getPlayer().getScore() > 21);
+        } else if (game.getStats().contains("0:1")) {
+            assertTrue(game.getPlayer().getScore() > 21);
         }
     }
 
@@ -53,20 +50,18 @@ public class BlackjackGameTest {
             assertEquals(2, game.getPlayer().getHand().size());
             assertEquals(21, game.getDealer().getScore());
             assertEquals(2, game.getDealer().getHand().size());
-        }
-
-        else if (game.getStats().contains("1:0")) {
-            if ((game.getDealer().getScore() > 17) && (game.getDealer().getScore() <=21) && (game.getDealer().getScore() != 21)) {
+        } else if (game.getStats().contains("1:0")) {
+            if ((game.getDealer().getScore() > 17)
+                && (game.getDealer().getScore() <= 21) && (game.getDealer().getScore() != 21)) {
                 assertTrue((game.getDealer().getScore()) < (game.getPlayer().getScore()));
             } else if (game.getPlayer().getScore() != 21) {
                 assertTrue((game.getDealer().getScore() > 21));
             } else {
                 assertTrue((game.getPlayer().getScore() == 21));
             }
-        }
-
-        else if (game.getStats().contains("0:1")) {
-            if ((game.getDealer().getScore() > 17) && (game.getDealer().getScore() <=21) && (game.getPlayer().getScore() != 21)) {
+        } else if (game.getStats().contains("0:1")) {
+            if ((game.getDealer().getScore() > 17)
+                && (game.getDealer().getScore() <= 21) && (game.getPlayer().getScore() != 21)) {
                 assertTrue((game.getDealer().getScore()) > (game.getPlayer().getScore()));
             } else {
                 assertTrue((game.getDealer().getScore() == 21));
